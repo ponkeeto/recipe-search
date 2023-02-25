f9498696dd980550b476f3bf7cc8133acaabad5b
ponkeeto <ponchsalvador@gmail.com>
2023-02-25 15:06:21 +0800

    add nutrients and ingredients info

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -9,7 +9,19 @@ import {
   Typography,
   Modal,
   Box,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Table,
+  TableContainer,
+  Paper,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
 } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 import { getRecipeInfo, selectRecipeInfo } from "./reducer";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -28,13 +40,10 @@ const style = {
 const RecipeCard = (props) => {
   const { id, image, title } = props;
   const [open, setOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const recipeInfo = useSelector(selectRecipeInfo);
   const dispatch = useDispatch();
-
-  const { recipeInfo: recipe, isLoading } = recipeInfo;
-  const { instructions, sourceUrl, nutrition } = recipe;
-  console.log(nutrition);
 
   const handleOpen = () => {
     dispatch(getRecipeInfo(id));
@@ -42,29 +51,215 @@ const RecipeCard = (props) => {
   };
   const handleClose = () => setOpen(false);
 
-  const renderModal = (
-    <Modal open={open} onClose={handleClose}>
-      <Box sx={style}>
-        {!isLoading ? (
-          <>
-            <Typography variant="h4" component="h2">
-              {title}
-            </Typography>
-            {instructions}
-            <a href={sourceUrl} target="_blank">
-              Check out the website
-            </a>
-          </>
-        ) : (
-          <>loading...</>
-        )}
-      </Box>
-    </Modal>
-  );
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const renderModal = () => {
+    const { recipeInfo: recipe, isLoading } = recipeInfo;
+    const {
+      instructions,
+      sourceUrl,
+      nutrition,
+      extendedIngredients: ingredients,
+    } = recipe;
+
+    const nutritionData = () => {
+      if (nutrition) {
+        const { caloricBreakdown, nutrients, weightPerServing } = nutrition;
+        return (
+          <TableContainer
+            component={Paper}
+            sx={{ maxHeight: "250px", overflow: "scroll" }}
+          >
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <b>Caloric Breakdown</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Amount</b>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.entries(caloricBreakdown).map(([key, value]) => {
+                  return (
+                    <TableRow
+                      key={key}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {key.slice(7)}
+                      </TableCell>
+                      <TableCell align="right">{value}%</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <b>
+                      Nutrients (per {weightPerServing.amount}
+                      {weightPerServing.unit} serving)
+                    </b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Amount</b>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {nutrients.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      {row.amount}
+                      {row.unit}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      }
+      return null;
+    };
+
+    const ingredientsData = () => {
+      if (ingredients) {
+        return (
+          <TableContainer
+            component={Paper}
+            sx={{ maxHeight: "250px", overflow: "scroll" }}
+          >
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <b>Ingredient</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Amount</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Preparation</b>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ingredients.map((row) => {
+                  const { nameClean: name, measures, meta } = row;
+                  return (
+                    <TableRow
+                      key={name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {name}
+                      </TableCell>
+                      <TableCell align="right">
+                        {measures.metric.amount} {measures.metric.unitShort}
+                      </TableCell>
+                      <TableCell align="right">
+                        {meta.length > 0 &&
+                          meta.map(
+                            (meta, index) => `${meta}${index + 1 < meta.length && ", "}`
+                          )}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      }
+      return null;
+    };
+
+    return (
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          {!isLoading ? (
+            <>
+              <Typography variant="h4" component="h2">
+                {title}
+              </Typography>
+              <Accordion
+                expanded={expanded === "panel1"}
+                onChange={handleChange("panel1")}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1bh-content"
+                  id="panel1bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Ingredients
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>{ingredientsData()}</AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === "panel2"}
+                onChange={handleChange("panel2")}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2bh-content"
+                  id="panel2bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Instructions
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography sx={{ maxHeight: "250px", overflow: "scroll" }}>
+                    {instructions}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === "panel3"}
+                onChange={handleChange("panel3")}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel3bh-content"
+                  id="panel3bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Nutrition
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>{nutritionData()}</AccordionDetails>
+              </Accordion>
+              <a href={sourceUrl} target="_blank">
+                Check out the website
+              </a>
+            </>
+          ) : (
+            <>loading...</>
+          )}
+        </Box>
+      </Modal>
+    );
+  };
 
   return (
     <>
-      {renderModal}
+      {renderModal()}
       <Card raised key={id} className="img" style={{ height: "100%" }}>
         <CardActionArea onClick={handleOpen}>
           <CardMedia component="img" height="250" image={image} alt={title} />
